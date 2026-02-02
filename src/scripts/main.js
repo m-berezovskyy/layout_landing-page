3f23b061adc2615a92764ab105ad987b1cb135c6
Marian <mberez999@gmail.com>
2026-02-02 15:04:26 +0200

    final work

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const burgerButton = document.querySelector('.header__icon--burger');
+const closeButton = document.querySelector('#menu__close');
+const menuOverlay = document.querySelector('#menu');
+
+burgerButton.addEventListener('click', () => {
+  menuOverlay.classList.add('is-open');
+  document.body.style.overflow = 'hidden';
+});
+
+closeButton.addEventListener('click', () => {
+  menuOverlay.classList.remove('is-open');
+  document.body.style.overflow = '';
+});
