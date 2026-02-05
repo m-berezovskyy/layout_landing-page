541a6367a2be06c8907a7c6885d5a1ebef91bcf7
Marian <mberez999@gmail.com>
2026-02-05 04:41:46 +0200

    final work 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,26 @@
 const burgerButton = document.querySelector('.header__icon--burger');
 const closeButton = document.querySelector('#menu__close');
 const menuOverlay = document.querySelector('#menu');
+const menuLinks = document.querySelectorAll('.menu__link');
+const form = document.querySelector('.footer__form');
 
 burgerButton.addEventListener('click', () => {
   menuOverlay.classList.add('is-open');
   document.body.style.overflow = 'hidden';
 });
 
-closeButton.addEventListener('click', () => {
+function closeMenu() {
   menuOverlay.classList.remove('is-open');
   document.body.style.overflow = '';
+}
+
+closeButton.addEventListener('click', closeMenu);
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', closeMenu);
+});
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  form.reset();
 });
